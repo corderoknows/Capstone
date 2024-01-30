facd31d305d2274b3cf70c298983f663a9ec35eb
Cordero W <129771033+corderoknows@users.noreply.github.com>
2024-01-30 16:24:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+export DATABASE_URL=postgres://cordero:rI1TmvavcNic5F1oOoDgjRDlkzLRr4FH@dpg-cmsk3kacn0vc73bj4oe0-a/casting_01eq
 export DATABASE_URL="postgresql://postgres@localhost:5432/postgres"
-export EXCITED="true"
-echo "setup.sh script executed successfully!"
+
+export FLASK_APP=app.py
+export AUTH0_DOMAIN='dev-iete7gtr2uo5uot6.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='capstone'
+export CLIENT_ID='yJLuLnnTiWoyrJnpmNfdn0SfUDd846v7'
+
+
